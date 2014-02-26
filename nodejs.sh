666434ee306b20da34731d175f0c88ee4406fe54
mavewei <mavewei@gmail.com>
2014-02-26 13:11:59 +0800

    Initial commit

---
+++ nodejs.sh
@@ -0,0 +1,13 @@
+sudo apt-get install python-software-properties python g++ make
+sudo apt-get install vim htop
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+npm install mysql@2.0.0-alpha9
+npm install json-socket
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo npm install -g nodemon
+sudo npm install forever -g
+sudo npm install --save moment
+
